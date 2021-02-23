310ccfb67c371ac4bc7721db74fd973bb824d897
Davidhfw <wuhaifenger@163.com>
2021-02-23 20:28:48 +0800

    Update hanlerchain.java

--- handlechain/hanlerchain.java
+++ handlechain/hanlerchain.java
@@ -35,3 +35,36 @@ public class HandlerB extends Handler {
 }
 
 // HandlerChain和Application代码不变
+public class HandlerChain {
+  private Handler head = null;
+  private Handler tail = null;
+
+  public void addHandler(Handler handler) {
+    handler.setSuccessor(null);
+
+    if (head == null) {
+      head = handler;
+      tail = handler;
+      return;
+    }
+
+    tail.setSuccessor(handler);
+    tail = handler;
+  }
+
+  public void handle() {
+    if (head != null) {
+      head.handle();
+    }
+  }
+}
+
+// 使用举例
+public class Application {
+  public static void main(String[] args) {
+    HandlerChain chain = new HandlerChain();
+    chain.addHandler(new HandlerA());
+    chain.addHandler(new HandlerB());
+    chain.handle();
+  }
+}
