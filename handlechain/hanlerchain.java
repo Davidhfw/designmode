8cfb5006333365f3ea987ce32632791830f9bb57
Davidhfw <wuhaifenger@163.com>
2021-02-23 20:26:49 +0800

    Create hanlerchain.java

---
+++ handlechain/hanlerchain.java
@@ -0,0 +1,37 @@
+
+public abstract class Handler {
+  protected Handler successor = null;
+
+  public void setSuccessor(Handler successor) {
+    this.successor = successor;
+  }
+
+  public final void handle() {
+    boolean handled = doHandle();
+    if (successor != null && !handled) {
+      successor.handle();
+    }
+  }
+
+  protected abstract boolean doHandle();
+}
+
+public class HandlerA extends Handler {
+  @Override
+  protected boolean doHandle() {
+    boolean handled = false;
+    //...
+    return handled;
+  }
+}
+
+public class HandlerB extends Handler {
+  @Override
+  protected boolean doHandle() {
+    boolean handled = false;
+    //...
+    return handled;
+  }
+}
+
+// HandlerChain和Application代码不变
